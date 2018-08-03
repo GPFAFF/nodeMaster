ed07c419cd74952c7f003e36f3d095431f3caeed
Greg Pfaff <greg.pfaff@razorfish.com>
2018-08-03 10:50:24 -0500

    adding req res object

--- app/index.js
+++ app/index.js
@@ -5,11 +5,32 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 
 // The server should respond to all requests with a string.
-
 const server = http.createServer((req, res) => {
+  console.log(req);
+  // Grab URL and parse it.
+  const parsedURL = url.parse(req.url, true);
+  // Get path from URL.
+  const path = parsedURL.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+  // Get query string as object
+
+  const queryStringObject = parsedURL.query;
+
+  // Get HTTP Method
+  const method = req.method.toLowerCase();
+
+  // Get headers as an object
+  const headers = req.headers;
+
+  // Send response from URL.
   res.end('Yo dude\n');
+  // Log what path the person was asking for.
+  console.log(`Request is received on this path: ${trimmedPath} with this method: ${method} and with this queryString`, queryStringObject);
+  console.log('Header requests', headers)
 });
 
 // Start the server, and have it listen on port.
