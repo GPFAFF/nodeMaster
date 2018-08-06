4649a2aef8a0f4bf95cc5983fe544ec9909fa321
Greg Pfaff <greg.pfaff@razorfish.com>
2018-08-06 11:38:33 -0500

    adding routing

--- app/index.js
+++ app/index.js
@@ -6,10 +6,10 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all requests with a string.
 const server = http.createServer((req, res) => {
-  console.log(req);
   // Grab URL and parse it.
   const parsedURL = url.parse(req.url, true);
   // Get path from URL.
@@ -26,11 +26,51 @@ const server = http.createServer((req, res) => {
   // Get headers as an object
   const headers = req.headers;
 
-  // Send response from URL.
-  res.end('Yo dude\n');
-  // Log what path the person was asking for.
-  console.log(`Request is received on this path: ${trimmedPath} with this method: ${method} and with this queryString`, queryStringObject);
-  console.log('Header requests', headers)
+  // Get payload, if user sends one
+  const decoder = new StringDecoder('utf-8');
+  let payLoad = '';
+  req.on('data', (data) => {
+    payLoad += decoder.write(data);
+  });
+  req.on('end', () => {
+    payLoad += decoder.end();
+
+    // Route proper handler, if one is not found go to NotFound Handler
+
+    const selectedHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+    // Construct data object to send to handler
+
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      payLoad
+    }
+
+    // Route the request to the handler specified in router
+    selectedHandler(data, (statusCode, payLoad) => {
+      // Use status code called back by handler, or default to 200
+      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+      // Use the payload called back by the handler or default to empty object.
+      payLoad = typeof(payLoad) == 'object' ? payLoad : {};
+
+      // Convert payLoad to string
+      const payLoadString = JSON.stringify(payLoad);
+
+      // Return the response
+      res.writeHead(statusCode);
+      res.end(payLoadString);
+      console.log('Returning this response', statusCode, payLoadString);
+       // Log what path the person was asking for.
+      // console.log(`Request is received on this path: ${trimmedPath} with this method: ${method} and with this queryString`, queryStringObject);
+      // // Log the headers sent
+      // console.log('Header requests', headers);
+      // console.log('Here is the payload:', payLoad)
+    });
+  });
 });
 
 // Start the server, and have it listen on port.
@@ -38,3 +78,22 @@ server.listen(4444, () => {
   console.log('Server is listening on PORT 4444');
 });
 
+// Define handlers
+
+var handlers = {};
+
+// Sample Handler
+handlers.sample = (data, callback) => {
+  // Callback and HTTP status code and a payload object
+  callback(406, {'name' : 'My name is Sample Handler'});
+
+}
+
+// 404 Handler
+handlers.notFound = (data, callback) => {
+  callback(404);
+}
+// Define a request router
+const router = {
+  'sample': handlers.sample
+}
